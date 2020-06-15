28f75c052e341c443ebe8116b6936d95bba5caa2
Nikolay Popov <niknikpopov@gmail.com>
2020-06-15 20:16:46 +0300

    Initial commit: project started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Header from './components/Header';
+import TableList from './components/tableLayout/TableList';
+import FormInput from './components/FormInput';
+import ButtonAbsolute from './components/ButtonAbsolute';
 
 function App() {
+  let list = [];
+  const [formIsOpen, toggleForm] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    list = localStorage.getItem('list') && JSON.parse(localStorage.getItem('list'));
+    return () => {
+      if (list && list.length > 0) {
+        localStorage.setItem('list', JSON.stringify(list));
+      } else {
+        localStorage.setItem('list', JSON.stringify([]));
+      }
+    }
+  }, [list])
+
+  const toggleModal = () => {
+    toggleForm(!formIsOpen)
+  }
+
+  const appendRecord = (data) => {
+    if (data) {
+      const arr = Object.entries(data);
+      list.append(arr);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <TableList list={list} formIsOpen={formIsOpen}/>
+      {formIsOpen && <FormInput list={list} appendRecord={appendRecord}/>}
+      <ButtonAbsolute formIsOpen={formIsOpen} toggleModal={toggleModal}/>
     </div>
   );
 }
